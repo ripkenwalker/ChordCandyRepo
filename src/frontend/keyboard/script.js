8d9e620f0c6fd7aff1d24b57f7036a53766ca0c6
emurray2 <evan@auraaudio.io>
2021-10-08 09:17:26 -0400

    Piano now works with Tonejs

--- src/frontend/keyboard/script.js
+++ src/frontend/keyboard/script.js
@@ -1,52 +1,4 @@
-// Store keyboard key names
-const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
-
-// Get all key elements in the HTML
-const keys = document.querySelectorAll('.key')
-const whiteKeys = document.querySelectorAll('.key.white')
-const blackKeys = document.querySelectorAll('.key.black')
-
-// What happens when a key on the keyboard is clicked
-keys.forEach(key => {
-    // Play a note
-    key.addEventListener('click', () => playNote(key))
-})
-
-// What happens when a key on the computer keyboard is pressed
-document.addEventListener('keydown', e => {
-    // If the key is held down, exit the function
-    if (e.repeat) return
-
-    // Get the computer keyboard event
-    const key = e.key
-
-    // Check which key on the computer keyboard is pressed
-    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-    const blackKeyIndex = BLACK_KEYS.indexOf(key)
-
-    // If a key was found, get this key in the HTML element and
-    // play its file
-    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
-    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
-})
-
-// Play the note audio
-function playNote(key) {
-    // Get the audio filename for the note
-    const noteAudio = document.getElementById(key.dataset.note)
-    // Set the note to start
-    noteAudio.currentTime = 0
-    noteAudio.play()
-    // Add an 'active' element to the classlist (for highlighting the keyboard)
-    key.classList.add('active')
-    // Add a listener for when the file finishes playing to remove the highlight
-    noteAudio.addEventListener('ended', () => {
-        key.classList.remove('active')
-    })
-}
-
-// Tone.js piano example code
+// Tone.js piano sampler
 const sampler = new Tone.Sampler({
     urls: {
         A0: "A0.mp3",
@@ -84,8 +36,69 @@ const sampler = new Tone.Sampler({
     baseUrl: "https://tonejs.github.io/audio/salamander/"
 }).toDestination();
 
-piano({
-    parent: document.querySelector("#content"),
-    noteon: note => sampler.triggerAttack(note.name),
-    noteoff: note => sampler.triggerRelease(note.name),
-});
+// Store keyboard key names
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+
+// Get all key elements in the HTML
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+function trackMouse() {
+}
+
+// What happens when a key on the keyboard is clicked
+keys.forEach(key => {
+    // Play a note
+    key.addEventListener('mousedown', () => playNote(key))
+    key.addEventListener('mouseup', () => releaseNote(key))
+})
+
+// What happens when a key on the computer keyboard is pressed
+document.addEventListener('keydown', e => {
+    // If the key is held down, exit the function
+    if (e.repeat) return
+
+    // Get the computer keyboard event
+    const key = e.key
+
+    // Check which key on the computer keyboard is pressed
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    // If a key was found, get this key in the HTML element and
+    // play its pitch
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
+
+//What happens when a key on the computer keyboard is released
+document.addEventListener('keyup', e => {
+    // Get the computer keyboard event
+    const key = e.key
+    // Check which key on the computer keyboard is released
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    // If a key was found, get this key in the HTML element and
+    // release its pitch
+    if (whiteKeyIndex > -1) releaseNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) releaseNote(blackKeys[blackKeyIndex])
+})
+
+// Play the note audio
+function playNote(key) {
+    const piano = document.querySelector('.piano')
+    piano.addEventListener('mousemove', trackMouse)
+    key.classList.add('active')
+    sampler.triggerAttack(key.dataset.note);
+}
+
+// Release the note audio
+function releaseNote(key) {
+    const piano = document.querySelector('.piano')
+    piano.removeEventListener('mousemove', trackMouse)
+    key.classList.remove('active');
+    sampler.triggerRelease(key.dataset.note);
+}
