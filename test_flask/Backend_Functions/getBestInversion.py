8b8db6f61b85be4414a5371175446f80523f6567
sabi1083 <sabi1083@gmail.com>
2021-11-17 11:31:03 -0500

    Path Fixes

--- test_flask/Backend_Functions/getBestInversion.py
+++ test_flask/Backend_Functions/getBestInversion.py
@@ -5,7 +5,7 @@ import json
 from Backend_Functions.chordIntSplit import chordIntSplit
 from Backend_Functions.chordIntCombine import chordIntCombine
 
-with open('Beach_Server\Backend_Functions\chordDictionary.json', 'r') as dictOpen:
+with open('Backend_Functions/chordDictionary.json', 'r') as dictOpen:
     nameToNum = json.load(dictOpen)
 
 def getBestInversion(inChord, destChord):
