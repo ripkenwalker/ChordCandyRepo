677e1141bfbb4cecb588d40a800800d899980dd8
ripkenwalker <90044954+ripkenwalker@users.noreply.github.com>
2021-12-03 10:11:54 -0500

    Deleted test_flask
    R.I.P. :santa:

--- test_flask/Backend_Functions/chordIntCombine.py
+++
@@ -1,4 +0,0 @@
-# Reverses chordIntSplit, will combine the individual note values back into a single integer
-def chordIntCombine (input):
-    combine = int(str(input[0]) + str(input[1]) + str(input[2]) + str(input[3]))
-    return combine
--- test_flask/Backend_Functions/getDictPath.py
+++
@@ -1,8 +0,0 @@
-# Get path for dictionary (this can change with the operating system)
-from pathlib import Path
-
-def getDictPath():
-
-    dictPath = Path.cwd() / 'test_flask' / 'Backend_Functions' / 'chordDictionaryNEW.json'
-
-    return dictPath
